91cf5af6565cb0f7f77dc7caff8a3f73f9902378
dungnv52tk <dungnv52tk@gmail.com>
2019-03-26 15:38:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let x, y, z, avg;
+x = prompt("Nhap vao diem mon Vat ly: ");
+y = prompt("Nhap vao diem mon Hoa hoc: ");
+z = prompt("Nhap vao diem mon Sinh hoc: ");
+let a = parseInt(x);
+let b = parseInt(y);
+let c = parseInt(z);
+avg = (a + b + c)/3;
+alert("Diem trung binh la: " + avg);
